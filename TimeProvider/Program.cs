ec97931df30c3208d6021788b913d92da647ecc8
Hary Prabowo Suryoatmojo <Hary.Suryoatmojo@unit4.com>
2022-08-10 08:29:11 +0700

    Create TimeProvider class.

--- TimeProvider/Program.cs
+++ TimeProvider/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TimeProvider timeProvider = new TimeProvider();
+            DateTime dateTimeNow = timeProvider.GetNextOneMinuteFromNow();
+            Console.WriteLine(dateTimeNow);
+        }
+    }
+
+    public class TimeProvider
+    {
+        public DateTime GetNextOneMinuteFromNow()
+        {
+            return DateTime.Now.AddMinutes(1);
         }
     }
 }
